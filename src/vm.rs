e24d7587408577156082e23780ef2e336a5c281b
1011X <1011XXXXX@gmail.com>
2016-03-19 16:56:23 -0400

    Removed commented example.

--- src/vm.rs
+++ src/vm.rs
@@ -15,44 +15,6 @@ pub fn vm(file_path: &Path) {
 	let mut reg = [0_u16; 16];
 	let mut mem = [0_u16; 65536];
 	
-	/*
-	Sample addition program that takes args a and b:
-	
-	let program = [
-		// addition using bit-ops
-		0x1001, // lit 0 r1   ; r1 = 0
-		0x1002, // lit 0 r2
-		0x0012, // not r2     ; r2 = 0xFFFF
-		
-		0x1023, // lit 2 r3   ; a = 2
-		0x1024, // lit 2 r4   ; b = 2
-		0x1005, // lit 0 r5   ; carry = 0
-		0x1006, // lit 0 r6   ; result = 0
-		
-		0x4345, // toff a b carry   ; carry = a & b
-		0x0234, // cnot a b         ; b ^= a
-		0x4246, // toff r2 b result ; result = b
-		0x0234, // cnot a b         ; b ^= a
-		0x0105, // swap r0 carry
-		
-		0x7018, // if r0 == 0, goto 24
-		
-		0x0105, // swap r0 carry
-		0x1007, // lit 0 r7   ; shiftedcarry = 0
-		0x1018, // lit 1 r8   ; temp = 1
-		0x0018, // not temp   ; temp = ~temp
-		0x0025, // rotl carry
-		0x4257, // toff temp carry shiftedcarry   ; shiftedcarry = carry & temp
-		0x1005, // lit 0 r5                       ; carry = 0
-		0x4675, // toff result shiftedcarry carry ; carry = result & shiftedcarry
-		0x0276, // cnot shiftedcarry result       ; result ^= shiftedcarry
-		0x0105, // swap r0 carry
-		0x600C, // jump 12
-		
-		0x0000  // halt
-	];
-	*/
-	
 	// read file contents into mem
 	let mut buffer = [0_u8; 2];
 	for i in 0.. {
