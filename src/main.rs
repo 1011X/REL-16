78b28c3be356745916a27af835cb3ae06bf71965
1011X <1011XXXXX@gmail.com>
2016-08-08 15:32:58 -0400

    Better CLI
    * Show help menu when command-line arguments are formatted incorrectly.

--- src/main.rs
+++ src/main.rs
@@ -23,18 +23,27 @@ enum Source {
 	File(File),
 }
 
+const USAGE: &'static str = "Usage:
+    rel [--version] [--help]
+    rel run [--verbose] <input>
+    rel [d]asm [--output <filepath>] <input>
+    
+    The special argument \"-\" can be used as input to specify stdin.";
+
 
 fn main() {
 	let args = env::args().skip(1);
 	
 	let mut opts = Options::new();
 	// shouldn't be required, bc `rel run` doesn't need it
-	opts.optopt("o", "output", "set output to FILENAME", "FILENAME");
-	opts.optflag("V", "version", "print program version");
-	opts.optflag("h", "help", "print this help menu");
-	opts.optflag("v", "verbose", "log each step the VM takes");
+	opts.optopt("o", "output",   "Set output destination to <filepath>. Default: \"default.bin\"", "<filepath>");
+	opts.optflag("V", "version", "Print program version");
+	opts.optflag("h", "help",    "Print this help menu");
+	opts.optflag("v", "verbose", "Log each step the VM takes");
 	
 	let matches = try_err!(opts.parse(args));
+	
+	let print_help = || println_err!("{}", opts.usage(USAGE));
 	
 	if matches.opt_present("version") {
 		println!("rel 0.2.5");
@@ -42,7 +51,7 @@ fn main() {
 	}
 	
 	if matches.opt_present("help") {
-		println!("{}", opts.usage(&opts.short_usage("rel")));
+		print_help();
 		return;
 	}
 	
@@ -51,16 +60,28 @@ fn main() {
 		Some("asm")  => Command::Assembler(asm::Action::Assemble),
 		Some("dasm") => Command::Assembler(asm::Action::Disassemble),
 		
-		Some(other) => panic!("Unrecognized subcommand: {}", other),
+		Some(other) => {
+			println_err!("Unrecognized subcommand: {}", other);
+			print_help();
+			return;
+		}
 		
-		None => panic!("Missing subcommand."),
+		None => {
+			println_err!("Missing subcommand.");
+			print_help();
+			return;
+		}
 	};
 	
 	let src = match matches.free.get(1).map(|s| s.as_str()) {
 		Some("-") => Source::Stdin,
 		Some(path) => Source::File(try_err!(File::open(path))),
 		
-		None => panic!("No input source given."),
+		None => {
+			println_err!("No input source given.");
+			print_help();
+			return;
+		}
 	};
 	
 	match command {
