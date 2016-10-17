423d85d6e561052d72acc14a925bc94e32ef0acd
1011X <1011XXXXX@gmail.com>
2016-10-17 11:33:36 -0400

    Renamed some mneumonics.
    * Swap-PC-with-register is now `spc` (was `sp`).
    * Reverse-and-swap-PC-with-register is now `rspc` (was `rsp`).

--- examples/call.asm
+++ examples/call.asm
@@ -1,16 +1,16 @@
 jmp 4
 
 ; push r0 (sp) r1 (val)
-sp r2
+spc r2
 inc r0
 xchg r1 r0
-sp r2
+spc r2
 ; pop r0 (sp) r1 (reg)
 
 pmj 4
 xori r0 255; sp = 255
 xori r1 3  ; val = 3
 xori r2 1  ; push = 0x0001 + 1
-sp r2
-rsp r2
+spc r2
+rspc r2
 hlt
