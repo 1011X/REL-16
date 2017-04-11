06ca611b1a1197e878e94a479b820ff0055f2131
1011X <1011XXXXX@gmail.com>
2017-04-11 00:14:29 -0400

    Improved comments on examples

--- examples/stack.asm
+++ examples/stack.asm
@@ -1,14 +1,24 @@
 ; uses built-in stack to push 2 numbers, then
 ; pops them and adds them
 
+; initialization
 not r6      ; sp = 0xffff
 not r7      ; bp = 0xffff
-inc r0      ; ++r0  // r0 = 1
+
+; summon value from the void and store it
+xori r0 5   ; r0 ^= 5  // r0 = 5
 push r0     ; push(r0, sp)
-inc r0      ; ++r0
-inc r0      ; ++r0  // r0 = 2
+
+; again
+xori r0 3   ; r0 ^= 3  // r0 = 3
 push r0     ; push(r0, sp)
-pop r0      ; pop(r0, sp)  // r0 = 2
-pop r1      ; pop(r1, sp)  // r1 = 1
-add r0 r1   ; r0 += r1
+
+; take values from stack
+pop r0      ; pop(r0, sp)  // r0 = 3
+pop r1      ; pop(r1, sp)  // r1 = 5
+
+; sum them
+add r0 r1   ; r0 += r1  // r0 = 8
+
+; result is in r0
 hlt
