e74d6aecb399112186e8307f7d8d0968fd0d3a6b
1011X <1011XXXXX@gmail.com>
2017-04-13 16:48:34 -0400

    Moved REL AST to its own crate `rel_isa` (again)

---
+++ rel_isa/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod op;
+pub mod reg;
+
+pub use op::{Op, Addr};
+pub use reg::Reg;
--- src/vm/register_file.rs
+++ src/vm/register_file.rs
@@ -1,6 +1,5 @@
 use std::ops::{Index, IndexMut};
-
-use isa::reg::Reg;
+use isa::Reg;
 
 pub struct RegisterFile(pub [u16; 8]);
 
